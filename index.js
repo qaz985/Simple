bddc7a3880437e74b161ca43f0cb64b05044ecf7
qaz985 <xsw985@naver.com>
2020-09-24 11:59:37 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://james:bond@cluster0.syqfo.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongDB Connected.."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
